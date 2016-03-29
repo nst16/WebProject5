e22e20521aa3c94190535ce1cf7e93c6c513d995
nst16 <nealsthacker@gmail.com>
2016-03-29 15:06:45 -0400

    added form validation

--- signup-submit.php
+++ signup-submit.php
@@ -3,6 +3,74 @@
 	</head>
 	<body>
 	<?php include 'top.html/top.html'; ?>
+	<?php
+		//validate the submitted input
+	
+		//check if name is blank
+		if($_POST["Name"] == "")
+		{
+			echo "Error: Name cannot be blank.";
+			exit();
+		}
+
+		//ago must be between 0 and 99
+		if($_POST["Age"] <= 0 || $_POST["Age"] >= 99)
+		{
+			echo "Error: Age cannot be outside of (0, 99).";
+			exit();
+		}
+
+		//gender must be m/f
+		if($_POST["Gender"] != "M" &&  $_POST["Gender"] != "F")
+		{
+			echo "Error: Gender must be male or female";
+			exit();
+		}
+
+		//personality type must contain correct letters
+		$letters = str_split($_POST["PersonalityType"]);
+		if($letters[0] != 'I' && $letters[0] != 'E')
+		{
+			if($letters[1] != 'S' && $letters[1] != 'N')
+			{
+				if($letters[2] != 'F' && $letters[2] != 'T')
+				{
+					if($letters[3] != 'P' && $letters[3] != 'J')
+					{
+						echo "Error: Invalid personality type";
+						exit();
+					}
+				}
+			}
+		}
+
+		//valid favorite OS
+		if($_POST["FavoriteOS"] != "Windows" && $_POST["FavoriteOS"] != "Mac OS X" && $_POST["FavoriteOS"] != "Linux")
+		{
+			echo "Error: Invalid Favorite OS";
+			exit();
+		}
+
+		//min/max age must be valid
+		if($_POST["MinAge"] < 0)
+		{
+			echo "Error: Invalid minimum age";
+			exit();
+		}
+
+		if($_POST["MaxAge"] > 99)
+		{
+			echo "Error: Invalid maximum age";
+			exit();
+		}
+
+		if($_POST["MinAge"] > $_POST["MaxAge"])
+		{
+			echo "Error: Minimum cannot be higher than the maximum age";
+			exit();
+		}
+
+	?>
 	<p> Thank you! </p>
 	<?php
 		echo 'Welcome to NerdLuv, ' . htmlspecialchars($_POST["Name"]) . '!';
