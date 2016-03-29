4e49cbc0867bb3a2ea8e91016c5ce2840fc0b90e
nst16 <nealsthacker@gmail.com>
2016-03-28 21:26:01 -0400

    fixed variable names

--- signup.php
+++ signup.php
@@ -7,11 +7,11 @@
         <form action="signup-submit.php" method="post">
 	  <fieldset> <legend>New User Signup:</legend>
             Name:<input type="text" maxlength="16" name="Name"> <br>
-            Gender:<input type="radio" name="Gender" value="Male"> Male
-            <input type="radio" name="Gender" value="Female"> Female <br>
+            Gender:<input type="radio" name="Gender" value="M"> Male
+            <input type="radio" name="Gender" value="F"> Female <br>
             Age: <input type="text" name="Age" size="6" maxlength="2"> <br>
-            Personality Type: <input type="text" size="6" maxlength="4" name="Personality Type"> (<a href=http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>) <br>
-            Favorite OS:  <select name="Favorite OS">
+            Personality Type: <input type="text" size="6" maxlength="4" name="PersonalityType"> (<a href=http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>) <br>
+            Favorite OS:  <select name="FavoriteOS">
 		<option value="Windows">Windows</option>
 		<option value="Mac OS X">Mac OS X</option>
 		<option value="Linux">Linux</option>
